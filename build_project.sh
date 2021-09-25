8f5d3db454d1cc823459f88f3956430880fd0ec0
Michał Sidor <public+git@meekchopp.es>
2021-09-25 13:52:53 +0200

    feat: github actions groups

--- build_project.sh
+++ build_project.sh
@@ -1,15 +1,26 @@
-#!/bin/sh -ex
+#!/bin/sh -e
 project="$1"
+echo "::group::Ensuring the repository exists"
 git clone "https://github.com/michcioperz/$project" --bare "$project.git" || true
+echo "::endgroup::"
 repo="$(realpath "$project.git")"
 tmpdir="$(mktemp -d -p "$repo")"
 function finish {
+	echo "::group::Removing the temporary work tree"
 	git -C "$repo" worktree remove "$tmpdir"
-	rmdir "$tmpdir" || true
+	echo "::endgroup::"
 }
 trap finish EXIT
+echo "::group::Fetching new changes"
 git -C "$repo" fetch
+echo "::endgroup::"
+echo "::group::Checking out HEAD"
 git -C "$repo" worktree add "$tmpdir" FETCH_HEAD
+echo "::endgroup::"
 cd "$tmpdir"
+echo "::group::Building"
 result="$(nix-build --no-out-link)"
+echo "::endgroup::"
+echo "::group::Deploying"
 nix-env --profile /nix/var/nix/profiles/per-user/nginx/michciooo -i "$result"
+echo "::endgroup::"
